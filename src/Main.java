96933ede2944e0a4e4fbe5e017d5a280b1acd080
rishikesh-revandikar <rishikeshrevandikar3110@gmail.com>
2024-11-15 19:51:57 +0530

    tested for user  creation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import com.rishikesh.library.User;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        User user = new User("Rishikesh Revandikar","abc@gmail.com","1234567890");
+        System.out.println("Name: "+user.getName());
+        System.out.println("Email: "+user.getEmail());
+        System.out.println("Phone Number: "+user.getPhoneNumber());
     }
 }
